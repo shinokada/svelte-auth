76afc54141ebfea212ece4a361bfdb0be04e9e46
Shinichi Okada <okada.shin@gmail.com>
2021-12-20 13:22:31 +0900

    Working on vercel send email.

--- src/lib/sendgridmailer.js
+++ src/lib/sendgridmailer.js
@@ -5,31 +5,61 @@ dotenv.config()
 const host = process.env['HOST_URL']
 const email_from = process.env['EMAIL_FROM']
 const name_from = process.env['NAME_FROM']
-
+const SENDGRID_API = 'https://api.sendgrid.com/v3/mail/send'
+const SENDGRID_API_KEY = process.env['SENDGRID_API']
 
 export const sendGridConfirmationEmail = async (name, email, confirmationCode) => {
-  await sgMail.setApiKey(process.env['SENDGRID_API'])
-  const msg = {
-    to: email,
-    from: `${name_from} <${email_from}>`,
-    subject: "Please confirm your account",
-    text: `Email Confirmation: Hello ${name}.
-    Please confirm your email by clicking on the following link.
-    Click here, ${host}/auth/confirm/${confirmationCode}`,
-    html:`<h1>Email Confirmation</h1>
-    <h2>Hello ${name}</h2>
-    <p>Please confirm your email by clicking on the following link.</p>
-    <a href=${host}/auth/confirm/${confirmationCode}> Click here</a>
-    </div>`,
-  }
-  await sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent')
+  await fetch(SENDGRID_API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${SENDGRID_API_KEY}`
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [
+            {
+              email
+            }
+          ],
+          subject: 'Demo success :)'
+        }
+      ],
+      from: {
+        email: 'noreply@demo.com',
+        name: 'Test SendGrid'
+      },
+      content: [
+        {
+          type: 'text/html',
+          value: `Congratulations <b>${name}</b>, you just sent an email with sendGrid. ${confirmationCode}`
+        }
+      ]
     })
-    .catch((error) => {
-      console.error(error)
-    })
+});
+  // await sgMail.setApiKey(process.env['SENDGRID_API'])
+  // const msg = {
+  //   to: email,
+  //   from: `${name_from} <${email_from}>`,
+  //   subject: "Please confirm your account",
+  //   text: `Email Confirmation: Hello ${name}.
+  //   Please confirm your email by clicking on the following link.
+  //   Click here, ${host}/auth/confirm/${confirmationCode}`,
+  //   html:`<h1>Email Confirmation</h1>
+  //   <h2>Hello ${name}</h2>
+  //   <p>Please confirm your email by clicking on the following link.</p>
+  //   <a href=${host}/auth/confirm/${confirmationCode}> Click here</a>
+  //   </div>`,
+  // }
+  // await sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     console.log('Email sent')
+  //   })
+  //   .catch((error) => {
+  //     console.error(error)
+  //   })
 }
 
 export const sendGridForgotEmail = (name, email, confirmationCode) => {
